a6be49ef48999a2e8e56e1c9186a4b8ecc9ccdbf
hat082 <2798638863@qq.com>
2024-06-18 20:31:44 +0800

    Script that copies files relevant to the project from personal notes
    Added a basic gitignore

---
+++ scripts/filter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the tag to search for
+TAG="#1-projects/FURP"
+
+# Define the source and destination directories
+SOURCE_DIR="/mnt/c/Users/jacky/Obsidian Notes/"
+DEST_DIR="/mnt/c/Users/jacky/FURP_LOG/"
+
+# Find and copy files with the specified tag, overwriting if the file already exists
+find "$SOURCE_DIR" -type f -exec grep -l "$TAG" {} \; | while read file; do
+	filename=$(basename "$file") # Get the filename from the path
+	copy_path="$DEST_DIR/$filename"
+	echo "Copying $file to $copy_path"
+	cp -f "$file" "$copy_path"
+done
+
+echo "File copying completed."
